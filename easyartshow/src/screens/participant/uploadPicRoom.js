cb2887837c8fd48b3a12a66ebe45857394b521e9
Christopher Le <locvicvn1234@gmail.com>
2023-02-24 13:06:03 -0600

    create upload picture button without backend

--- easyartshow/src/screens/participant/uploadPicRoom.js
+++ easyartshow/src/screens/participant/uploadPicRoom.js
@@ -1,14 +1,35 @@
-import React from 'react'
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-function UploadPicRoom() {
-    const navigate = useNavigate();
-    return (
-      <div>
-        Upload picture: <input type="file" />
-        <button onClick={() => navigate('/waitingroom')}>Upload</button>
-      </div>
-    );
-  }
-  
+const UploadPicRoom = () => {
+  const [picture, setPicture] = useState(null);
+  const [progress, setProgress] = useState(0);
+  const [imageUrl, setImageUrl] = useState("");
+  const navigate = useNavigate();
+
+  const handlePictureChange = (e) => {
+    const file = e.target.files[0];
+    const reader = new FileReader();
+    reader.onload = () => {
+      setImageUrl(reader.result);
+    };
+    reader.readAsDataURL(file);
+  };
+
+  return (
+    <div>
+      Choose a picture:
+      <input type="file" onChange={handlePictureChange} />
+      {imageUrl && (
+        <img
+          src={imageUrl}
+          alt="Selected"
+          style={{ width: "150px", height: "150px" }}
+        />
+      )}
+      <div>{progress}%</div>
+    </div>
+  );
+};
+
 export default UploadPicRoom;
