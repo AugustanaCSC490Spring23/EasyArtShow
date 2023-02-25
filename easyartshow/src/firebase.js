50fba2d27aabf43bc7705e947aaeb72734540af8
Nhi <hnhi.trangia@gmail.com>
2023-02-24 18:45:53 -0600

    fix: login popup

--- easyartshow/src/firebase.js
+++ easyartshow/src/firebase.js
@@ -1,13 +1,13 @@
 // Import the functions you need from the SDKs you need
-import * as firebase from 'firebase';
+// import * as firebase from 'firebase';
 import { getAnalytics } from "firebase/analytics"; 
 import { getStorage } from "@firebase/storage";
 import { initializeApp } from "firebase/app";
 import { getAuth } from "@firebase/auth";
 
 const app = initializeApp({
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
+  apiKey: "AIzaSyBV_B_qv5tkl3gYwXFuAhPs06A2qRRwHaA",
+  authDomain: "easyartshow-2dbd2.firebaseapp.com",
   projectId: process.env.PROJECT_ID,
   storageBucket: process.env.STORAGE_BUCKET,
   messagingSenderId: process.env.MESSAGE_SENDER_ID,
@@ -15,6 +15,8 @@ const app = initializeApp({
   measurementId: process.env.MEASUREMENT_ID,
 });
 
-const storage = getStorage(app);
-const auth = getAuth(app);
-export default storage;
+export const storage = getStorage(app);
+// const auth = getAuth(app);
+
+// export default storage;
+export const auth = getAuth(app);
--- easyartshow/src/screens/host/authentication/login.js
+++ easyartshow/src/screens/host/authentication/login.js
@@ -8,41 +8,43 @@ import {
   GoogleAuthProvider,
   getAuth,
 } from "@firebase/auth";
-
+import { auth } from '../../../firebase';
 
 function Login() {
   const navigate = useNavigate();
   const provider = new GoogleAuthProvider();
   // const auth = getAuth();
 
-  // const loginWithGoogle = () => {
-  //   signInWithPopup(auth, provider)
-  //     .then((result) => {
-  //       // This gives you a Google Access Token. You can use it to access the Google API.
-  //       const credential = GoogleAuthProvider.credentialFromResult(result);
-  //       const token = credential.accessToken;
-  //       // The signed-in user info.
-  //       const user = result.user;
-  //       // IdP data available using getAdditionalUserInfo(result)
-  //       // ...
-  //     })
-  //     .catch((error) => {
-  //       // Handle Errors here.
-  //       const errorCode = error.code;
-  //       const errorMessage = error.message;
-  //       // The email of the user's account used.
-  //       const email = error.customData.email;
-  //       // The AuthCredential type that was used.
-  //       const credential = GoogleAuthProvider.credentialFromError(error);
-  //       // ...
-  //     });
-  // };
+  console.log(auth);
+  const loginWithGoogle = async () => {
+    const loginAttempt = await signInWithPopup(auth, provider)
+      .then((result) => {
+        console.log('success');
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+        // The signed-in user info.
+        const user = result.user;
+        // IdP data available using getAdditionalUserInfo(result)
+        // ...
+      })
+      .catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const email = error.customData.email;
+        // The AuthCredential type that was used.
+        const credential = GoogleAuthProvider.credentialFromError(error);
+        // ...
+      });
+  };
 
   return (
     <div>
       Login
       <button onClick={() => navigate("/dashboard")}> Get started </button>
-      <button > Login with Google </button>
+      <button onClick={loginWithGoogle}> Login with Google </button>
     </div>
   );
 }
