0b1359a86b1d6550a69e40e6fa36a38f41c5d45d
Christopher Le <locvicvn1234@gmail.com>
2023-03-07 10:29:25 -0600

    Update images.js

--- easyartshow/src/constants/images.js
+++ easyartshow/src/constants/images.js
@@ -1,21 +1,21 @@
 import thumbnail from '../asset/Thumbnail.png';
 import logo from '../asset/EAS-logo.png';
 import gallery01 from '../asset/img-1.jpg';
-import gallery02 from '../asset/img-2.jpg';
-import gallery03 from '../asset/img-3.jpg';
-import gallery04 from '../asset/img-4.jpg';
-import gallery05 from '../asset/img-5.jpg';
-import gallery06 from '../asset/img-6.png';
-import gallery07 from '../asset/img-7.png';
+// import gallery02 from '../asset/img-2.jpg';
+// import gallery03 from '../asset/img-3.jpg';
+// import gallery04 from '../asset/img-4.jpg';
+// import gallery05 from '../asset/img-5.jpg';
+// import gallery06 from '../asset/img-6.png';
+// import gallery07 from '../asset/img-7.png';
 
 export default {
     thumbnail,
     logo,
     gallery01,
-    gallery02,
-    gallery03,
-    gallery04,
-    gallery05,
-    gallery06,
-    gallery07
+    // gallery02,
+    // gallery03,
+    // gallery04,
+    // gallery05,
+    // gallery06,
+    // gallery07
 };
