b83b495e2c81ec613eeae1a7dd1e619568e4ebdd
Christopher Le <locvicvn1234@gmail.com>
2023-05-25 20:28:41 -0500

    Add about, dev note, and contact us page

--- easyartshow/src/App.js
+++ easyartshow/src/App.js
@@ -11,6 +11,7 @@ import Login from './screens/host/authentication/login';
 // import SignUp from './screens/host/authentication/signup';
 import MainPage from './screens/mainpage';
 import Dashboard from './components/Dashboard/dashboard';
+import ContactUs from './screens/contact/contactUs';
 import WaitingRoom from './components/Room/WaitingRoom/waitingroom';
 import CreateRoom from './components/Room/CreateRoom/createroom';
 import HostRoom from './screens/host/hostroom';
@@ -20,7 +21,7 @@ import UploadWithAI from './screens/participant/uploadWithAI';
 import DevNote from './screens/devnote';
 import Map from './components/Map';
 import PageNotFound from './screens/pageNotFound';
-import About from './screens/about';
+import About from './screens/about/about';
 import QRCodeComponent from './components/QRCodeComponent';
 import SlideShow from './components/Room/SlideShow/SlideShow';
 
@@ -42,7 +43,8 @@ function App() {
             <Route path="/map" element={<Map />} />
             <Route path="/404" element={<PageNotFound />} />
             <Route path="*" element={<Navigate to="/404" />} />
-            <Route path="/contact" element={<About />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<ContactUs />} />
             <Route path="/qrcode/:id" element={<QRCodeComponent />} />
             <Route path="/slideshow/:id" element={<SlideShow/>} />
         </Routes>
